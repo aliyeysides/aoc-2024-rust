d3c361a7643cb6fa17f19336634316cd630ff698
Ali Yeysides <ali.yeysides@gmail.com>
2024-12-06 17:13:20 -0500

    refactor regex

--- src/day_03/mod.rs
+++ src/day_03/mod.rs
@@ -1,18 +1,14 @@
 use regex::Regex;
 
 pub fn solution_part_1(mem: &str) -> u32 {
-    let re = Regex::new(r"(mul\(\d+,\d+\))").unwrap();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let mut total = 0;
 
-    for (_, [m]) in re.captures_iter(mem).map(|c| c.extract()) {
-        let numre = Regex::new(r"(\d+),(\d+)").unwrap();
+    for (_, [d1, d2]) in re.captures_iter(mem).map(|c| c.extract()) {
+        let num1: u32 = d1.parse().expect("match should be a u32");
+        let num2: u32 = d2.parse().expect("match should be a u32");
 
-        for (_, [d1, d2]) in numre.captures_iter(m).map(|c| c.extract()) {
-            let num1: u32 = d1.parse().expect("match should be a u32");
-            let num2: u32 = d2.parse().expect("match should be a u32");
-
-            total += num1 * num2;
-        }
+        total += num1 * num2;
     }
 
     total
