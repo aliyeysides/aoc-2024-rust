74756d22d3549d85a20e2d9b70ed101bddb72509
Ali Yeysides <ali.yeysides@gmail.com>
2024-12-06 14:27:30 -0500

    day 2 part 2

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -30,19 +30,18 @@ pub fn solution_part_1(file: &Path) -> u32 {
         for i in 1..args.len() {
             let prev: u32 = args[i - 1];
             let curr: u32 = args[i];
-            let mut _direction: Direction = Direction::NONE;
 
-            if prev > curr {
-                _direction = Direction::DESC;
+            let direction = if prev > curr {
+                Direction::DESC
             } else {
-                _direction = Direction::ASC;
-            }
+                Direction::ASC
+            };
 
-            if _direction != prev_direction && i > 1 {
+            if direction != prev_direction && prev_direction != Direction::NONE {
                 continue 'line;
             }
 
-            prev_direction = _direction;
+            prev_direction = direction;
 
             let diff = prev.abs_diff(curr);
 
@@ -52,6 +51,57 @@ pub fn solution_part_1(file: &Path) -> u32 {
         }
 
         safe_count += 1;
+    }
+
+    safe_count
+}
+
+pub fn solution_part_2(file: &Path) -> u32 {
+    let reader = File::open(file)
+        .map(BufReader::new)
+        .expect("File should exist.");
+
+    let mut safe_count = 0;
+
+    'line: for line in reader.lines() {
+        let line = line.unwrap();
+        let args: Vec<u32> = line
+            .split_whitespace()
+            .map(|v| v.parse().expect("value should be a number."))
+            .collect();
+
+        'variant: for i in 0..args.len() {
+            let mut new_vec = args.clone();
+            new_vec.remove(i);
+
+            let mut prev_direction: Direction = Direction::NONE;
+
+            for i in 1..new_vec.len() {
+                let prev: u32 = new_vec[i - 1];
+                let curr: u32 = new_vec[i];
+
+                let direction = if prev > curr {
+                    Direction::DESC
+                } else {
+                    Direction::ASC
+                };
+
+                if direction != prev_direction && prev_direction != Direction::NONE {
+                    continue 'variant;
+                };
+
+                prev_direction = direction;
+
+                let diff = prev.abs_diff(curr);
+
+                if diff < 1 || diff > 3 {
+                    continue 'variant;
+                }
+            }
+
+            safe_count += 1;
+            continue 'line
+        }
     }
 
     safe_count
@@ -67,5 +117,12 @@ mod tests {
         let path = Path::new("src/day_02/test_input.txt");
         let solution = solution_part_1(path);
         assert_eq!(solution, 2);
+    }
+
+    #[test]
+    fn tolerant_safe_count() {
+        let path = Path::new("src/day_02/test_input.txt");
+        let solution = solution_part_2(path);
+        assert_eq!(solution, 4);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::path::Path;
 
-use aoc_2024::day_02::solution_part_1;
+use aoc_2024::day_02::solution_part_2;
 
 fn main() {
     let path = Path::new("src/day_02/input.txt");
-    let ans = solution_part_1(path);
+    let ans = solution_part_2(path);
     println!("{}", ans);
 }
